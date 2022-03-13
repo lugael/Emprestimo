ae69fec7e8f756c82d9658121bb7117befe3cb97
lugael <79815839+lugael@users.noreply.github.com>
2022-03-13 19:49:00 -0300

    Add files via upload

--- Calculo.js
+++ Calculo.js
@@ -1,4 +1,7 @@
+
+var total = '';
 $(document).ready(function() {
+
     $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
           if (this.hash !== "") {
               event.preventDefault();
@@ -10,10 +13,12 @@ $(document).ready(function() {
                   });
             }
   });
-    var juros = 1.0019;
+    var juros = 1.019;
     var valor = '';
     var parcelas = '';
-    $('#valor, #nParcelas').on('change input', function(e) {
+    var vpar='';
+
+    $('.cadastroCliente').on('change', function(e) {
 
         valor = Number($('#valor').val().replace(/\./g, '').replace(',', '.') || 0);
         parcelas = Number($('#nParcelas').val());
@@ -23,7 +28,9 @@ $(document).ready(function() {
             return false
         };
 
-        $('#vParcela').text('R$ ' + ( juros ** parcelas * valor / parcelas).toLocaleString('pt-br'));
+        vpar =  ( juros ** parcelas * valor / parcelas);
+        total = parseFloat(vpar) * parseInt($('#nParcelas').val());
+        $('#vParcela').text('R$' +  vpar.toFixed(2));
         mostraResumo();
 
     });
@@ -31,10 +38,21 @@ $(document).ready(function() {
 });
 
 
-
 function mostraResumo() {
 
-    var html = '<table style="margin:1em; padding:2em;">';
+    var html = '<div class="contrato" >' +
+                    '<span> Eu<b> ' +$("#nomec").val()+ ' </b>portador do <b>CPF : ' +$("#cpf").val()+ '</b> nascido em <b>' +$("#ddn").val()+
+                        '</b> desejo realizar a contratação de um Empréstimo Pessoal, que será enviado no <b> PIX: ' + $('#chave').val() +'</b>.<br>'+
+                        'valor Emprestado: <b>R$ ' +$("#valor").val()+ '</b>.<br>' +
+                        'parcelado em <b>' +$("#nParcelas").val()+ '</b> vezes.<br>' +
+                        'Vencimento ao dia <b>'+ $("#vVencimento").val()+'</b> de cada mês.<br>'+
+                        'Resultando o valor mensal a pagar de:<b> R$' +$("#vParcela").text()+ '</b>.<br>' +
+                        'Taxa de Juros : 1,99% a.m. <br>'+
+                        'Valor total:<b> R$' + total.toFixed(2) + '</b><br>'+
+
+                    '</span>'
+                '</div>'
+
 
     var today = new Date();
     var mes= '';
@@ -44,13 +62,11 @@ function mostraResumo() {
     $(".resumo").empty();
     mes = today.getMonth()
     ano = today.getFullYear()
+    html += '<table style="margin:1em; padding:2em;">';
+
     for (var i = 1; i <= $("#nParcelas").val(); i++) {
 
-        console.log()
-
-        console.log(mes)
         if(mes >= 12) {
-            console.log('entrou no if')
             mes = 1;
             ano += 1;
         } else {
@@ -59,7 +75,6 @@ function mostraResumo() {
 
         date = ano+'-'+mes+'-'+$("#vVencimento").val();
         parc = ""+ $("#vParcela").text();
-
 
 
         html += ""
@@ -71,5 +86,34 @@ function mostraResumo() {
     }
 
     $(".resumo").append(html)
+
+}
+
+function verificarCadastro() {
+
+    if($("#cpf").val()  == '') return false;
+    if($("#ddn").val() == '') return false;
+    if($("#valor").val() == '') return false;
+    if($("#nParcelas").val() == '') return false;
+    if($("#vVencimento").val() == '') return false;
+
+return true;
+
+}
+
+
+function limparCadastro() {
+
+    if(!verificarCadastro()) return false;
+
+    alert('Emprestimo enviado para validação de dados logo entraremos em contato por email!');
+
+    $("#cpf").val('');
+    $("#ddn").val('');
+    $("#valor").val('');
+    $("#nParcelas").val('');
+    $("#vVencimento").val('');
+
+    $(".resumo").empty();
 
 }
